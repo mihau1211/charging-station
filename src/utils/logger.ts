bd9ea1cce7f920b963eda2e691aad5d4720dae17
mwrobel <michal.wrobel@ttpsc.pl>
2023-09-28 20:36:59 +0200

    feat: add generic funtions, change to class

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,14 +1,66 @@
 import winston from 'winston';
 
-const logger = winston.createLogger({
-    level: 'info',
-    format: winston.format.combine(
-        winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
-        winston.format.printf(info => `[${info.timestamp} [${info.level.toUpperCase()}] [${info.context || 'App'}]]: ${info.message}`)
-    ),
-    transports: [
-        new winston.transports.Console()
-    ],
-});
+class Logger {
+    private logger: winston.Logger;
+
+    constructor() {
+        this.logger = winston.createLogger({
+            level: 'info',
+            format: winston.format.combine(
+                winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+                winston.format.printf(info => `[${info.timestamp}] [${info.level.toUpperCase()}]: ${info.message}`)
+            ),
+            transports: [
+                new winston.transports.Console()
+            ],
+        });
+    }
+
+    beginLogger(method: string, endpoint: string, body: any) {
+        this.logger.info(`${method} request received at ${endpoint} ${!body ? '' : `with body: ${JSON.stringify(body)}`}`);
+    }
+
+    postSuccessLogger(model: string) {
+        this.logger.info(`${model} successfully created.`);
+    }
+
+    postErrorLogger(model: string, errorMessage: string) {
+        this.logger.error(`Error occurred while creating ${model}: ${errorMessage}`);
+    }
+
+    getSuccessLogger(model: string, where: any, limit: string, offset: string) {
+        this.logger.info(`Fetching ${model} with conditions: ${JSON.stringify(where)}, limit: ${limit}, offset: ${offset}`);
+    }
+
+    getErrorLogger(model: string, errorMessage: string) {
+        this.logger.error(`Error occurred while fetching ${model}: ${errorMessage}`);
+    }
+
+    getByIdErrorLogger(model: string, errorMessage: string) {
+        this.logger.error(`Error occurred while fetching ${model}: ${errorMessage}`);
+    }
+
+    idNotFoundLogger(model: string, id: string) {
+        this.logger.error(`${model} not found for ID: ${id}`);
+    }
+
+    invalidFieldsErrorLogger(endpoint: string) {
+        this.logger.error(`Invalid fields in request to ${endpoint}`);
+    }
+
+    patchSuccessLogger(model: string, id: string) {
+        this.logger.info(`${model} with id: ${id} successfully updated`);
+    }
+
+    constraintViolationErrorLogger(model: string, id: string, errorMessage: string) {
+        this.logger.error(`Unique constraint violation updating ${model} with id: ${id} - ${errorMessage}`);
+    }
+    
+    patchInternalErrorLogger(model: string, id: string, errorMessage: string) {
+        this.logger.error(`Internal Server Error updating ${model} with id: ${id} - ${errorMessage}`);
+    }
+}
+
+const logger = new Logger();
 
 export default logger;
